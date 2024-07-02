bfa9c9ab3c2200c347eea9b1e13f6957e1762446
Thiago <thiagofduarte12@gmail.com>
2024-07-02 15:18:50 -0300

    Alteração da página de login

--- routes/login.js
+++ routes/login.js
@@ -1,48 +1,51 @@
-// Importações
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 const adminModel = require('../models/AdminModel.js');
-const { userValidationRules, validateUser } = require('../validators/userValidator.js');
+const { loginValidationRules, validateData } = require('../validators/validators.js');
 
-// Rota GET para a página de login '/login'
+// Rota para renderizar a página de login
 router.get('/login', (req, res) => {
-    // Verifica se o usuário já está autenticado e redireciona para '/admin'
-    if (req.session.authenticated) res.redirect('/admin')
+    // Verifica se o usuário já está autenticado e redireciona para admin
+    if (req.session.authenticated) res.redirect('/admin');
     else {
-        // Caso contrário, obtém a mensagem de erro da sessão
-        const errorMessage = req.session.errorMessage
-        // Limpa a mensagem de erro da sessão após renderizar a página para o erro sumir se a página for recarregada
-        req.session.errorMessage = null
-        // Renderiza a página de login com a mensagem de erro (se houver)
-        res.render('login', { error: errorMessage })
+        // Renderiza a página de login com o token CSRF e mensagem de erro, se houver
+        const errorLogin = req.session.errorLogin;
+        delete req.session.errorLogin;
+        res.render('login', { csrfToken: req.csrfToken(), error: errorLogin, hasError: errorLogin != null });
     }
 })
 
-// Rota POST para o processo de login '/login'
-// Utiliza middleware userValidationRules e validateUser para validar as entradas do usuário
-router.post('/login', userValidationRules(), validateUser, (req, res) => {
-    // Obtém os dados do formulário de login
-    const { user, password } = req.body
-    // Verifica se as credenciais são válidas usando o método do AdminModel
+// Rota para processar o formulário de login
+router.post('/login', loginValidationRules(), validateData, (req, res) => {
+    if (req.errorMessages) {
+        // Se houver erro de validação, armazena mensagem de erro na sessão
+        req.session.errorLogin = req.errorMessages;
+        // Redireciona de volta para a página de login
+        return res.redirect('/login');
+    }
+    // Obtém os dados do formulário
+    const { user, password } = req.body;
+    // Verifica se as credenciais são válidas
     if (adminModel.validateCredentials(user, password)) {
         // Define a sessão como autenticada e armazena o usuário na sessão
-        req.session.authenticated = true
-        req.session.user = user
+        req.session.authenticated = true;
+        req.session.admin = adminModel.findAdmin(user);
         // Redireciona para a página admin após o login bem-sucedido
-        res.redirect('/admin')
+        res.redirect('/admin');
     } else {
-        // Caso contrário, define uma mensagem de erro na sessão e redireciona de volta para '/login'
-        req.session.errorMessage = "Falha ao realizar login."
-        res.redirect('/login')
+        // Define uma mensagem de erro na sessão
+        req.session.errorLogin = 'Falha ao realizar login.';
+        // Redireciona de volta para login
+        res.redirect('/login');
     }
 })
 
-// Rota GET para o processo de logout '/logout'
+// Rota para o processo de logout
 router.get('/logout', (req, res) => {
     // Destroi a sessão
-    req.session.destroy()
-    // Redireciona de volta para a página home após o logout
-    res.redirect('/')
+    req.session.destroy();
+    // Redireciona para a página inicial
+    res.redirect('/');
 })
 
-module.exports = router
+module.exports = router;
