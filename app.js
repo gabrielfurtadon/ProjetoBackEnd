7210a4ee704a4fe8f9642069c79eba242026aef2
Thiago <thiagofduarte12@gmail.com>
2024-07-02 14:46:21 -0300

    Alterações de sintaxe e inserção de middleware csrf

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 // Configuração do path e das variáveis de ambiente
-const path = require("path")
-require("dotenv").config()
+const path = require("path");
+require("dotenv").config();
 
 // Importações e configurações do Express
 const express = require('express');
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Configuração do engine de visualização Mustache
-const mustacheExpress = require("mustache-express")
-const engine = mustacheExpress()
-app.engine("mustache", engine)
+const mustacheExpress = require("mustache-express");
+const engine = mustacheExpress();
+app.engine("mustache", engine);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'mustache');
 
@@ -26,19 +26,24 @@ app.use(session({
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { expires: 60000 }
+  cookie: { maxAge: 300000 }
 }));
+
+// Middleware CSRF
+const csrf = require('csurf');
+const csrfProtection = csrf({ cookie: true });
+app.use(csrfProtection);
 
 // Importação do modelo AdminModel e adição do usário admin
 const adminModel = require('./models/AdminModel.js');
-const { LOGIN, PASSWORD } = process.env
-adminModel.addAdministrator(LOGIN, PASSWORD)
+const { LOGIN, PASSWORD } = process.env;
+adminModel.addAdministrator(LOGIN, PASSWORD);
 
 // Rotas da aplicação
 app.use('/', require('./routes/home'));
 app.use('/', require('./routes/login'));
 app.use('/', require('./routes/admin'));
-app.use('/', require('./routes/creation'));
+app.use('/', require('./routes/page'));
 
 // Middleware para lidar com erros na aplicação
 app.use(function (err, req, res, next) {
--- helpers/access.js
+++ helpers/access.js
@@ -1,16 +1,12 @@
 module.exports = {
-    // Função middleware que verifica se o usuário está autenticado
+    // Middleware para verificar se o usuário está autenticado
     isLogged: (req, res, next) => {
-        // Verifica se a propriedade authenticated da sessão é verdadeira (ou seja, se o usuário está autenticado)
-        if (req.session.authenticated) {
-            // Se o usuário estiver autenticado, a função 'next()' é chamada, permitindo que o fluxo continue para o próximo middleware ou rota
-            return next()
-        }
+        if (req.session.authenticated) next();
         else {
-            // Se o usuário não estiver autenticado, uma mensagem de erro é armazenada na sessão
-            req.session.errorMessage = "Usuário não autenticado"
-            // Redireciona o usuário para a rota "/login"
-            res.redirect("/login")
+            // Se não estiver autenticado, define uma mensagem de erro na sessão
+            req.session.errorLogin = "Usuário não autenticado";
+            // Redireciona o usuário para a pagina de login
+            res.redirect("/login");
         }
     }
 }
